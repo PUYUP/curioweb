88e150d809fc21c77e79dc162d4f4e682fc8f340
Rahman <hellopuyup@gmail.com>
2026-07-21 16:46:21 +0700

    feat: hardcode ATLA-Bronze slug and remove unused process import in auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/client";
 import { polarClient } from "@polar-sh/better-auth/client";
-import { env } from "process";
 
 export const authClient = createAuthClient({
     plugins: [polarClient()],
@@ -19,6 +18,9 @@ export const signInWithGoogle = async () => {
 
 // checkout
 export const checkout = async () => {
-    const checkoutResult = await authClient.checkout();
+    const checkoutResult = await authClient.checkout({
+        slug: 'ATLA-Bronze',
+    });
+
     console.log("checkoutResult", checkoutResult);
 }
