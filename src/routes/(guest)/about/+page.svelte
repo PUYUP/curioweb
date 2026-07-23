a142aa133762a4645c4278672d9842708c691055
Rahman <hellopuyup@gmail.com>
2026-07-23 15:47:15 +0700

    feat: add About Us page to provide mission and platform information

---
+++ src/routes/(guest)/about/+page.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>ATLANIZE - About Us</title>
+	<meta name="description" content="About Us - ATLANIZE" />
+	<meta
+		name="keywords"
+		content="about,ATLANIZE,research, curiosity, science, AI, bioinformatics, computational
+		bology, data analysis, technology, education"
+	/>
+	<meta name="author" content="ATLANIZE" />
+</svelte:head>
+
+<div class="flex flex-1 items-center justify-center px-4 py-8 md:py-24">
+	<div class="w-full max-w-lg">
+		<p>
+			The idea behind the name <u>"<i>A</i>TLANIZE"</u> is a platform where people can explore and discover
+			new things with research papers as medium. Today in the world million of papers wait for read. We
+			try to make it accessible to everyone through new way.
+		</p>
+
+		<p>
+			Artificial Intelligence is revolutionizing the world and change our way to learn something
+			new. Thats why we use AI to help people to explore and discover new things with research
+			papers base on thier interests automatically.
+		</p>
+
+		<p>
+			The difference <i>A</i>TLANIZE within current platforms is:
+		</p>
+
+		<ol class="list-decimal pl-8 mt-4">
+			<li>
+				<p>
+					Every two days you will receive two research papers. One paper base on your interests. And
+					the another one is opponent.
+				</p>
+			</li>
+
+			<li>
+				<p>
+					Read both paper then write essay. Challenge yourself to find what the crucial points of
+					both papers may can work together.
+				</p>
+			</li>
+
+			<li>
+				<p>Submit the essay. AI will grade your essay and give you points.</p>
+				<p><strong>Structure of the Observed Learning Outcome</strong></p>
+				<ol class="list-disc pl-8">
+					<li>
+						<p>Prestructural</p>
+					</li>
+					<li>
+						<p>Multistructural</p>
+					</li>
+					<li>
+						<p>Extended Abstract</p>
+					</li>
+					<li>
+						<p>Unistructural</p>
+					</li>
+					<li>
+						<p>Relational</p>
+					</li>
+				</ol>
+
+				<br />
+
+				<p><strong>Multidimensional Cognitive Assessment</strong></p>
+				<ol class="list-disc pl-8">
+					<li>
+						<p>Content Mastery</p>
+					</li>
+					<li>
+						<p>Critical Thinking</p>
+					</li>
+					<li>
+						<p>Cognitive Synthesis</p>
+					</li>
+					<li>
+						<p>Cognitive Stretch</p>
+					</li>
+					<li>
+						<p>Logical Flow</p>
+					</li>
+					<li>
+						<p>Academic Language</p>
+					</li>
+					<li>
+						<p>Memory Retention</p>
+					</li>
+				</ol>
+			</li>
+		</ol>
+
+		<br />
+
+		<p>
+			You can see. We help you build brain with paper. Not only knowledge but also skills. So you
+			cannot use this for cheat. It help you build brain and get better job. It is good for
+			humanity.
+		</p>
+
+		<p>If you have any question, feel free to contact us.</p>
+	</div>
+</div>
+
+<style>
+	p + p {
+		margin-top: 20px;
+	}
+</style>
