77bc88d6e2e28df463194f784e6dad04c38410d3
Rahman <hellopuyup@gmail.com>
2026-07-30 10:23:49 +0700

    feat: add drawer to view context chunks in workspace page

--- src/routes/(user)/workspaces/[id]/contexts/[context_id]/+page.svelte
+++ src/routes/(user)/workspaces/[id]/contexts/[context_id]/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import { Button } from '@/lib/components/ui/button';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import Icon from 'mdi-svelte';
+	import { mdiArrowRight } from '@mdi/js';
 	import type { PageServerData } from './$types';
 
 	const { data }: { data: PageServerData } = $props();
 
 	let { context } = $derived(data);
+
+	// chunks viewer
+	let chunkDrawerOpen = $state(false);
+	function viewChunkHandler() {
+		chunkDrawerOpen = !chunkDrawerOpen;
+	}
 
 	// svelte-ignore state_referenced_locally
 	console.log(context);
@@ -27,5 +36,33 @@
 
 	<div class="w-full xl:w-3/6 pb-4">
 		<p>{context?.content}</p>
+
+		<div class="flex gap-4 mt-6">
+			{#if context?.chunks && context.chunks.length > 0}
+				<Button variant="outline" size="sm" onclick={viewChunkHandler}>
+					View {context.chunks.length} Chunks <Icon path={mdiArrowRight} size={0.65} />
+				</Button>
+			{/if}
+		</div>
 	</div>
 </div>
+
+<Drawer.Root bind:open={chunkDrawerOpen} direction="right">
+	<Drawer.Content>
+		<Drawer.Header>
+			<Drawer.Title>Context Chunkings</Drawer.Title>
+		</Drawer.Header>
+		<div class="p-2 overflow-y-auto">
+			<ol class="list-decimal pl-6">
+				{#each context?.chunks as chunk}
+					<li class="mb-2">
+						<p class="text-sm">{chunk?.content}</p>
+					</li>
+				{/each}
+			</ol>
+		</div>
+		<Drawer.Footer>
+			<Drawer.Close>Close</Drawer.Close>
+		</Drawer.Footer>
+	</Drawer.Content>
+</Drawer.Root>
