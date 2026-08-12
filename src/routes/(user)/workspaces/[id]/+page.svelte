6ff4ad5fe44c77434e8a233a5e667d02fc339f21
Rahman <hellopuyup@gmail.com>
2026-08-12 23:18:45 +0700

    style: update workspace research context grid to be responsive with additional breakpoints

--- src/routes/(user)/workspaces/[id]/+page.svelte
+++ src/routes/(user)/workspaces/[id]/+page.svelte
@@ -151,7 +151,9 @@
 			<span class="text-sm">Research Contexts</span>
 		</div>
 
-		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+		<div
+			class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4"
+		>
 			{#if contexts.length > 0}
 				{#each contexts as context}
 					<Card.Root>
